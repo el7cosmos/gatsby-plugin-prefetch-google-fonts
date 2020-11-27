12a31d14a59c5ae8f6a502bc66a74f340fc295ad
el7cosmos <el@elabee.me>
2020-11-27 13:33:07 +0700

    Concat css

--- plugins/export/merge-css.js
+++ plugins/export/merge-css.js
@@ -5,41 +5,6 @@ const {
 const CleanCss = require('clean-css');
 const path = require('path');
 
-function getLines(data) {
-  const lines = {};
-  data.split('\n').forEach((line) => {
-    line = line.trim();
-    if (line.indexOf('src:') === 0) {
-      const items = line
-        .replace(';', '')
-        .replace('src:', '')
-        .split(',')
-        .map((item) => item.trim());
-      const name = items.shift();
-      lines[name] = {
-        items,
-        line: line.replace(';', ''),
-      };
-    }
-  });
-  return lines;
-}
-
-function mergeLines(origin, newLines) {
-  Object.keys(origin).forEach((key) => {
-    if (newLines[key]) {
-      origin[key].newLine = `${origin[key].newLine || origin[key].line}, ${newLines[key].items[newLines[key].items.length - 1]}`;
-    }
-  });
-}
-
-function createCssString(str, obj) {
-  Object.keys(obj).forEach((key) => {
-    str = str.replace(obj[key].line, obj[key].newLine);
-  });
-  return str;
-}
-
 module.exports = async (config) => {
   const cachedPath = path.join('./.cache/google-fonts/', config.pathPrefix);
 
@@ -49,11 +14,7 @@ module.exports = async (config) => {
     readFile(`${cachedPath}/woff.css`, 'utf8'),
   ]);
 
-  const woff2Lines = getLines(woff2);
-  const woffLines = getLines(woff);
-  mergeLines(woff2Lines, woffLines);
-
-  let css = createCssString(woff2, woff2Lines);
+  let css = woff2 + woff;
   css = new CleanCss().minify(css).styles;
 
   // Add font-display swap as recommended here https://css-tricks.com/font-display-masses/.
